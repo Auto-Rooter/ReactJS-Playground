d5ee9ee31931e9dc0ad79a16e3d177530dbcd282
Hadi Assalem <hassalem@diamonddiagnostics.com>
2022-02-14 12:17:52 +0100

    Project_0: inline check

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import ReactDOM from 'react-dom';
 const year = 2022;
 const greeting = (<div><h1>Hello</h1><p>React in {year}</p> </div>);
 const rootNode = document.getElementById('root');
+const isAuthenticated = true;
 
 function Header(props) {
     return <h1>Hello {props.username}</h1>;
 }
 
+function Login() {
+    return <p>Please Login!</p>
+}
+
+function Logout() {
+    return <button>Sign Out</button>
+}
 
 function Layout(props) {
     return <div style={{ background: 'palegoldenrod' }}>
@@ -17,7 +25,7 @@ function Layout(props) {
 }
 
 ReactDOM.render(<Layout>
-    <Header username="Auto">
-        <footer>Copyright 2022</footer>
-    </Header>
+    {isAuthenticated ? <Header username="Auto" /> : <Login />}
+    {isAuthenticated && <Logout />}
+    <footer>Copyright 2022</footer>
 </Layout>, rootNode)
